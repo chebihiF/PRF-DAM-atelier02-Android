86d2bc1d02f70c27dabb18617b2a4c0f28b6a693
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-03 14:05:52 +0100

    Initial commit

---
+++ app/src/main/java/org/m2i/atelier02/MainActivity.java
@@ -0,0 +1,14 @@
+package org.m2i.atelier02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
